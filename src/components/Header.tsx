11eb7c9e51aed60163c6377eb74c139fb54f177e
Sergey <myliveweb@mail.ru>
2021-05-04 08:51:51 +0300

    'Fix'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -132,7 +132,7 @@ const Header: React.FC = (props) => {
         <Toolbar className={classes.toolbar}>
           <Grid item xs={12} sm={6} md={4}>
             <Button size="small" onClick={handleBack}>
-              Назад 2
+              Назад
             </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
